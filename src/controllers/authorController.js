c45799b93e9a5e8f48b1382486ada964e8dae5f9
Subhadipa <bsubhadipa@gmail.com>
2021-11-29 21:24:42 +0530

    Project-1 in 29.11.2021-Latest Updated

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,7 +3,18 @@ const authorModel= require("../models/authorModel")
 const createAuthor= async function (req, res) 
 {
     var data= req.body
-    let savedData= await authorModel.create(data)
-    res.send({msg: savedData})    
+    const verifyEmail=req.body.email
+    const regx=(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    if(regx.test(verifyEmail))
+    {
+        let savedData= await authorModel.create(data)
+         res.send({msg: savedData})  
+        
+    }
+    else
+    {
+        res.send({msg:"Invalid email!"});
+    }
+      
 }
 module.exports.createAuthor= createAuthor
--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,22 +2,32 @@ const mongoose = require('mongoose')
 const authorModel= require("../models/authorModel")
 const blogModel= require("../models/blogModel")
 
-const createBlog = async function (req, res) {
-    const Id = req.body.id
-
-    const validId = await authorModel.findById(Id)
+const createBlog = async function (req, res) 
+{
+    
 
     const blogData = req.body;
-     
-    if (validId){
+     if(blogData.isPublished==true)
+     {
+        blogData["publishedAt"]=new Date()
+     }
+     const Id = req.body.authorId
+     const validId = await authorModel.findById(Id)
+    if (validId)
+    {
         const newBlog = await blogModel.create(blogData)
         res.status(201).send({status: true, msg: 'New blog created successfully', data: newBlog})
-     }else {
+     }
+     else 
+     {
         res.status(400).send({status: false, msg: 'Invalid Request'})
      }    
 }
 
+
 const getBlog=async function(req,res)
+{
+try
 {
     const blogData=await blogModel.find({isDeleted:false,isPublished:true})
     if(blogData)
@@ -27,9 +37,20 @@ const getBlog=async function(req,res)
     else
     {
         res.status(404).send({status: false,msg:"No documents found!"})
-    }  
+    }
 }
+catch(err)
+    {
+     
+      res.status(500).send({msg:"Some error occured"});
+    }
+}
+
+
+
 const getFilterBlog=async function(req,res)
+{
+try
 {
     const authorId=req.query.authorId
     const category=req.query.category
@@ -43,7 +64,13 @@ const getFilterBlog=async function(req,res)
     else
     {
         res.status(404).send({status: false,msg:"No documents found!"})
-    }  
+    }
+}  
+    
+catch(err)
+    {
+     res.status(500).send({msg:"Some error occured"});
+    }
 }
 
 module.exports.createBlog= createBlog
